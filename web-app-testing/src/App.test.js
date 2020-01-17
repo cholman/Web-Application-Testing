22274e253c40c78aa9eebc99a2c296177716ac2e
cholman <holman.cody@gmail.com>
2020-01-16 19:36:18 -0600

    testing

--- web-app-testing/src/App.test.js
+++ web-app-testing/src/App.test.js
@@ -1,9 +1,24 @@
 import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
+import * as rtl from '@testing-library/react';
+import Display from './components/Display';
+import Dashboard from './components/Dashboard';
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+test('renders without crashing', () => {
+    const page = rtl.render(<App/>)
+    expect(page).toBeVisible
+})
+
+test('renders display component', () => {
+    const { getByText } = rtl.render(<Display />);
+    const displayElement = getByText(/strikes/i);
+    expect(displayElement).toBeInTheDocument();
+})
+
+test('renders dashboard', () => {
+    const { getByText } = rtl.render (<Dashboard />);
+    const dashboardElement = getByText(/ball/i);
+    expect(dashboardElement).toBeInTheDocument();
+})
+
